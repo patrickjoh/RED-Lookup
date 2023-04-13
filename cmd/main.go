757de65918651907fa27d7a06eb56c33d82d3829
Sara <sarasdj@stud.ntnu.no>
2023-04-13 16:14:37 +0200

    update: main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,27 @@
-package cmd
+package main
+
+import (
+	"Assignment2"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	// Retrieve potential environment variable
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Println("$PORT has not been set. Default: " + Assignment2.DEFAULT_PORT)
+		port = Assignment2.DEFAULT_PORT
+	}
+
+	http.HandleFunc("/"+Assignment2.DEFAULT_PATH, handler.EmptyHandler)
+	http.HandleFunc("/"+Assignment2.HISTORY_PATH, handler.HandlerHistory)
+	http.HandleFunc("/"+Assignment2.NOTIFICATION_PATH, handler.HandlerNotifications)
+	http.HandleFunc("/"+Assignment2.STATUS_PATH, handler.HandlerStaus)
+	http.HandleFunc("/"+Assignment2.CURRENT_PATH, handler.HandlerRenewables)
+
+	log.Println("Listening on port " + port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
