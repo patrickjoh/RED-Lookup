47207ad7d46f43702b6f6c9fb97873e845680be6
Sara <sarasdj@stud.ntnu.no>
2023-04-13 16:44:47 +0200

    fix: missing Assignmen2 before constants

--- handler/defaultHandler.go
+++ handler/defaultHandler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"Assignment2"
 	"fmt"
 	"log"
 	"net/http"
@@ -16,13 +17,13 @@ func EmptyHandler(w http.ResponseWriter, r *http.Request) {
 	output := "<h1>Welcome!<h1><h3>This service does not provide any functionality on root path level." +
 		" Please try one of the paths below<h3>" +
 		"<h5 style=\"background-color: lightblue; width: 250px;\">Search for current percentage of renewables:<br>" +
-		"<a href=\"" + CURRENT_PATH + "\">" + CURRENT_PATH + "</a></h5>" +
+		"<a href=\"" + Assignment2.CURRENT_PATH + "\">" + Assignment2.CURRENT_PATH + "</a></h5>" +
 		"<h5 style=\"background-color: lightblue; width: 250px;\">Search for historical percentage of renewables:<br>" +
-		"<a href=\"" + HISTORY_PATH + "\">" + HISTORY_PATH + "</a></h5>" +
+		"<a href=\"" + Assignment2.HISTORY_PATH + "\">" + Assignment2.HISTORY_PATH + "</a></h5>" +
 		"<h5 style=\"background-color: lightblue; width: 250px;\">Search for notification endpoint:<br>" +
-		"<a href=\"" + NOTIFICATION_PATH + "\">" + NOTIFICATION_PATH + "</a></h5>" +
+		"<a href=\"" + Assignment2.NOTIFICATION_PATH + "\">" + Assignment2.NOTIFICATION_PATH + "</a></h5>" +
 		"<h5 style=\"background-color: lightblue; width: 250px;\">For status:<br>" +
-		"<a href=\"" + STATUS_PATH + "\">" + STATUS_PATH + "</a></h5>"
+		"<a href=\"" + Assignment2.STATUS_PATH + "\">" + Assignment2.STATUS_PATH + "</a></h5>"
 
 	// Write output to client
 	_, err := fmt.Fprintf(w, "%v", output)
