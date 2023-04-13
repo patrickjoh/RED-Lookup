09c0a8bddedb39c5ea8cc5b8127c724061d022b8
patrickjoh <patrick.johannessen@hotmail.com>
2023-04-13 17:35:50 +0200

    feat: Implemented StubHandler

--- stub/StubHandler.go
+++ stub/StubHandler.go
@@ -1,1 +1,34 @@
 package stub
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+// Define a struct that represents the structure of your JSON data
+type Country struct {
+	Name string `json:"name"`
+	Code string `json:"code"`
+}
+
+func StubHandler(w http.ResponseWriter) {
+	// Set the content type to JSON
+	w.Header().Set("Content-Type", "application/json")
+
+	// Parse the JSON file
+	output := parseFile("./res/RESTCountry.json")
+
+	// Finally, let's write the JSON to the response
+	w.Write(output)
+}
+
+func parseFile(filename string) []byte {
+	// Open and
+	file, e := os.ReadFile(filename)
+	if e != nil {
+		fmt.Printf("File error: %v\n", e)
+		os.Exit(1)
+	}
+	return file
+}
