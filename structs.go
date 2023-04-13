0970c74cdc9af0a3a71f43961604c59cbe830dd7
Hoa Ben The Nguyen <hbnguye@stud.ntnu.no>
2023-04-13 15:11:22 +0200

    add: structs for country and webhooks

--- structs.go
+++ structs.go
@@ -1,9 +1,23 @@
 package Assignment2
 
-type statusStruct struct {
+type statusData struct {
 	countries_api   string
 	notification_db string
 	webhooks        string
 	version         string
 	uptime          string
+}
+
+type countData struct {
+	name       string
+	isoCode    string
+	year       int
+	percentage float64
+}
+
+type webhookData struct {
+	webhook_id string
+	url        string
+	country    string
+	calls      int
 }
