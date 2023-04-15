9e809182b9f36064a2aa230784201d759c72221b
Hoa Ben The Nguyen <hbnguye@stud.ntnu.no>
2023-04-15 14:43:02 +0200

    change: fit with the new function

--- handler/renewHistory.go
+++ handler/renewHistory.go
@@ -58,16 +58,19 @@ func HandelHistoryGet(w http.ResponseWriter, r *http.Request) {
 	// Send a response with the extracted values
 	fmt.Fprintf(w, "Searching for: country = %s, year (%s - %s)", count, begin, end)
 
-	contries := convertCsvData() // Acquire data from csv-file
+	country := findCountry(convertCsvData(), count) // Acquire data from csv-file
 
 	var countData []Assignment2.CountryData // empty list for the final data
 	startYear, _ := strconv.Atoi(begin)     // beginning year
 	endYear, _ := strconv.Atoi(end)         // end year
 
-	fmt.Println(contries[0].Name)
+	for _, no := range country {
+		fmt.Println(no.Name)
+	}
+
 	// loops through the csv-file and return the data that was asked for
-	for _, col := range contries {
-		if col.Name == count && col.Year < endYear && col.Year > startYear {
+	for _, col := range country {
+		if col.Year < endYear && col.Year > startYear {
 			newHisData := Assignment2.CountryData{
 				Name:       col.Name,
 				IsoCode:    col.IsoCode,
