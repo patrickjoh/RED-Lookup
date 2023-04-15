cc59e62f736636dfec9a76f54e40817c02240c8c
Magnus Johannessen <magnuswj@stud.ntnu.no>
2023-04-15 16:15:31 +0200

    feat: initializing firebase notifications.go

--- handler/notifications.go
+++ handler/notifications.go
@@ -1,13 +1,56 @@
 package handler
 
 import (
+	"cloud.google.com/go/firestore"
+	"context"
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
+	"log"
 	"net/http"
 )
+
+// Firebase context and client used by Firestore functions throughout the program.
+var ctx context.Context
+var client *firestore.Client
+
+// Collection name in Firestore
+const collection = "messages"
+
+// Message counter to produce some variation in content
+var ct = 0
 
 // HandlerStudent main handler for route related to `/students` requests
 // Note: here we are using a higher-order function with closure, to propagate a reference
 // to the DB down the processing pipeline.
 func HandlerNotifications(w http.ResponseWriter, r *http.Request) {
+	// Firebase initialisation
+	ctx = context.Background()
+
+	// We use a service account, load credentials file that you downloaded from your project's settings menu.
+	// It should reside in your project directory.
+	// Make sure this file is git-ignored, since it is the access token to the database.
+	sa := option.WithCredentialsFile("assignment2-group11-firebase-adminsdk-lo4w1-716a00f3f5.json")
+	app, err := firebase.NewApp(ctx, nil, sa)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Instantiate client
+	client, err = app.Firestore(ctx)
+
+	// Check whether there is an error when connecting to Firestore
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Close down client at the end of the function
+	defer func() {
+		err := client.Close()
+		if err != nil {
+			log.Fatal("Closing of the Firebase client failed. Error:", err)
+		}
+	}()
+
 	switch r.Method {
 	case http.MethodPost:
 		handleNotificationsPost(w, r)
