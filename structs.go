108f3857006130d5964ed94f19a37553de291c04
Sara <sarasdj@stud.ntnu.no>
2023-04-16 15:20:39 +0200

    update: add back NotificationDB field in StatusData struct

--- structs.go
+++ structs.go
@@ -1,10 +1,11 @@
 package Assignment2
 
 type StatusData struct {
-	CountriesAPI string `json:"countries_api"`
-	Webhooks     int    `json:"webhooks"`
-	Version      string `json:"version"`
-	Uptime       string `json:"uptime"`
+	CountriesAPI   string `json:"countries_api"`
+	NotificationSB string `json:"notifications_db"`
+	Webhooks       int    `json:"webhooks"`
+	Version        string `json:"version"`
+	Uptime         string `json:"uptime"`
 }
 
 type CountryData struct {
