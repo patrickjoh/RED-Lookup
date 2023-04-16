f7abff9749c06a840cdde3afee98a18c83c6e39c
Sara <sarasdj@stud.ntnu.no>
2023-04-16 13:35:27 +0200

    add: TestGetOneCountry to currentRenew_test.go

--- handler/currentRenew_test.go
+++ handler/currentRenew_test.go
@@ -27,3 +27,26 @@ func TestGetAllCountries(t *testing.T) {
 
 	assert.Equal(t, expected, result)
 }
+
+func TestGetOneCountry(t *testing.T) {
+	// Sample data
+	data := []Assignment2.CountryData{
+		{Name: "Country1", Year: 2000, IsoCode: "20", Percentage: 1.5},
+		{Name: "Country1", Year: 2005, IsoCode: "20", Percentage: 54.2},
+		{Name: "Country2", Year: 2000, IsoCode: "40", Percentage: 13.2},
+		{Name: "Country2", Year: 2010, IsoCode: "40", Percentage: 42.2},
+		{Name: "Country3", Year: 2010, IsoCode: "50", Percentage: 99.1},
+	}
+
+	// Expected output
+	expected := []Assignment2.CountryData{
+		{Name: "Country1", Year: 2005, IsoCode: "20", Percentage: 54.2},
+		{Name: "Country2", Year: 2010, IsoCode: "40", Percentage: 42.2},
+	}
+
+	isoCodes := []string{"20", "40"}
+
+	result := getOneCountry(data, isoCodes)
+
+	assert.Equal(t, expected, result)
+}
