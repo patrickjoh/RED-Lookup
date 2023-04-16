84667616ff97d8db6aad70c9646375ad54284536
Sara <sarasdj@stud.ntnu.no>
2023-04-16 13:30:13 +0200

    add: currentRenew_test.go

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module Assignment2
 go 1.15
 
 require (
-	cloud.google.com/go/firestore v1.9.0 // indirect
-	firebase.google.com/go v3.13.0+incompatible // indirect
-	google.golang.org/api v0.118.0 // indirect
+	cloud.google.com/go/firestore v1.9.0
+	firebase.google.com/go v3.13.0+incompatible
+	github.com/stretchr/testify v1.8.1
+	google.golang.org/api v0.118.0
 )
---
+++ handler/currentRenew_test.go
@@ -0,0 +1,29 @@
+package handler
+
+import (
+	"Assignment2"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGetAllCountries(t *testing.T) {
+	// Sample data
+	data := []Assignment2.CountryData{
+		{Name: "Country1", Year: 2000, IsoCode: "20", Percentage: 1.5},
+		{Name: "Country1", Year: 2005, IsoCode: "20", Percentage: 54.2},
+		{Name: "Country2", Year: 2000, IsoCode: "40", Percentage: 13.2},
+		{Name: "Country2", Year: 2010, IsoCode: "40", Percentage: 42.2},
+		{Name: "Country3", Year: 2010, IsoCode: "50", Percentage: 99.1},
+	}
+
+	// Expected output
+	expected := []Assignment2.CountryData{
+		{Name: "Country1", Year: 2005, IsoCode: "20", Percentage: 54.2},
+		{Name: "Country2", Year: 2010, IsoCode: "40", Percentage: 42.2},
+		{Name: "Country3", Year: 2010, IsoCode: "50", Percentage: 99.1},
+	}
+
+	result := getAllCountries(data)
+
+	assert.Equal(t, expected, result)
+}
