b0efb822558c8da5c7f8522a65eecda0c1ed3adb
Sara <sarasdj@stud.ntnu.no>
2023-04-17 15:36:41 +0200

    update: retrieveDocument

--- handler/notifications.go
+++ handler/notifications.go
@@ -5,11 +5,12 @@ import (
 	"cloud.google.com/go/firestore"
 	"context"
 	firebase "firebase.google.com/go"
+	"fmt"
 	"google.golang.org/api/iterator"
 	"google.golang.org/api/option"
+	"io"
 	"log"
 	"net/http"
-	"strings"
 )
 
 // Firebase context and client used by Firestore functions throughout the program.
@@ -56,27 +57,47 @@ func NotificationsHandler(w http.ResponseWriter, r *http.Request) {
 
 	switch r.Method {
 	case http.MethodPost:
-		handleNotificationsPost(w, r)
+		addDocument(w, r)
 	case http.MethodGet:
-		handleNotificationsGet(w, r)
+		retrieveDocument(w, r)
 	case http.MethodDelete:
 		handleNotificationsDelete(w, r)
 	}
 }
 
-func handleNotificationsPost(w http.ResponseWriter, r *http.Request) {
+func addDocument(w http.ResponseWriter, r *http.Request) {
+	// Read body
+	text, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Println("Reading payload from body failed.")
+		http.Error(w, "Reading payload failed.", http.StatusInternalServerError)
+		return
+	}
 
-}
-
-func handleNotificationsGet(w http.ResponseWriter, r *http.Request) {
-	parts := strings.Split(strings.TrimSuffix(r.URL.Path, "/"), "/")
-
-	if len(parts) == 4 {
-		// Create webhook
-	} else if len(parts) == 5 {
-		// View webhook
+	log.Println("Received request to add document for content ", string(text))
+	if len(string(text)) == 0 {
+		log.Println("Content appears to be empty.")
+		http.Error(w, "Your payload (to be stored as document) appears to be empty. Ensure to terminate URI with /.", http.StatusBadRequest)
 	} else {
-		// Handle error
+		// Add element in embedded structure.
+		id, _, err := client.Collection(collection).Add(ctx,
+			map[string]interface{}{
+				"text": string(text),
+				"ct":   ct,
+				"time": firestore.ServerTimestamp,
+			})
+		ct++
+		if err != nil {
+			// Error handling
+			log.Println("Error when adding document " + string(text) + ", Error: " + err.Error())
+			http.Error(w, "Error when adding document "+string(text)+", Error: "+err.Error(), http.StatusBadRequest)
+			return
+		} else {
+			// Returns document ID in body
+			log.Println("Document added to collection. Identifier of returned document: " + id.ID)
+			http.Error(w, id.ID, http.StatusCreated)
+			return
+		}
 	}
 }
 
@@ -88,31 +109,59 @@ func postPayload(payload interface{}) {
 
 }
 
-func retrieveDocument(id string) {
+// retrieveAllDocuments retrieves all registered webhooks
+func retrieveAllDocuments() {
 
 }
 
-// GetNumWebhooks retrieves and returns the number of registered webhooks from Firestore
-func GetNumWebhooks() int {
-	// Get context and client
-	ctx, client = GetContextAndClient()
+// retrieveDocument a webhook specified by an id
+func retrieveDocument(w http.ResponseWriter, r *http.Request) {
+	// Get id from RUL
+	id := r.URL.Query().Get("id")
 
-	// Create reference to webhook collection in Firestore
-	webhooksCollection := client.Collection("webhooks")
+	// Extract individual message
+	if len(id) != 0 {
 
-	// Retrieve all webhooks from db
-	iter := webhooksCollection.Documents(context.Background())
-	var numWebhooks int
-	for {
-		_, err := iter.Next()
-		if err == iterator.Done {
-			break
+		// Retrieve specific message based on id (Firestore-generated hash)
+		res := client.Collection(collection).Doc(id)
+
+		// Retrieve reference to document
+		doc, err2 := res.Get(ctx)
+		if err2 != nil {
+			log.Println("Error extracting body of returned document of message " + id)
+			http.Error(w, "Error extracting body of returned document of message "+id, http.StatusInternalServerError)
+			return
 		}
-		if err != nil {
-			panic(err)
+
+		// A message map with string keys. Each key is one field, like "text" or "timestamp"
+		m := doc.Data()
+		_, err3 := fmt.Fprintln(w, m["text"])
+		if err3 != nil {
+			log.Println("Error while writing response body of message " + id)
+			http.Error(w, "Error while writing response body of message "+id, http.StatusInternalServerError)
+			return
 		}
-		numWebhooks++
+	} else {
+		// Collective retrieval of messages
+		iter := client.Collection(collection).Documents(ctx) // Loop through all entries in collection "messages"
+
+		for {
+			doc, err := iter.Next()
+			if err == iterator.Done {
+				break
+			}
+			if err != nil {
+				log.Fatalf("Failed to iterate: %v", err)
+			}
+			// Note: You can access the document ID using "doc.Ref.ID"
+
+			// A message map with string keys. Each key is one field, like "text" or "timestamp"
+			m := doc.Data()
+			_, err = fmt.Fprintln(w, m)
+			if err != nil {
+				log.Println("Error while writing response body (Error: " + err.Error() + ")")
+				http.Error(w, "Error while writing response body (Error: "+err.Error()+")", http.StatusInternalServerError)
+			}
+		}
 	}
-
-	return numWebhooks
 }
