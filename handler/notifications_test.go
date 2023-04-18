3c7758051863e04320c67560fcf03c7ee844acb2
Sara <sarasdj@stud.ntnu.no>
2023-04-18 23:08:15 +0200

    add: notifications_test.go

---
+++ handler/notifications_test.go
@@ -0,0 +1,18 @@
+package handler
+
+import "testing"
+
+func TestAddDocument(t *testing.T) {
+
+	testGetDocument()
+	testDeleteDocument()
+
+}
+
+func testDeleteDocument() {
+
+}
+
+func testGetDocument() {
+
+}
