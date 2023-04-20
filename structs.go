8df7be072c5cdef276241fadee19823d62728348
Sara <sarasdj@stud.ntnu.no>
2023-04-20 12:09:44 +0200

    change: NotificationSB field from int to string

--- structs.go
+++ structs.go
@@ -2,7 +2,7 @@ package Assignment2
 
 type StatusData struct {
 	CountriesAPI   string `json:"countries_api"`
-	NotificationSB int    `json:"notifications_db"`
+	NotificationSB string `json:"notifications_db"`
 	Webhooks       int    `json:"webhooks"`
 	Version        string `json:"version"`
 	Uptime         string `json:"uptime"`
