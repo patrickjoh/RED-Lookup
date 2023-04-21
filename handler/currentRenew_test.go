f5b0ca13b8d6dce496cdd2bcc52727cc2469d58e
patrickjoh <patrick.johannessen@hotmail.com>
2023-04-21 10:30:26 +0200

    fix: Fixed typos in the country-names

--- handler/currentRenew_test.go
+++ handler/currentRenew_test.go
@@ -8,8 +8,8 @@ import (
 
 // Sample data for test functions
 var sampleData = []Assignment2.CountryData{
-	{Name: "Azerbajan", IsoCode: "AZE", Year: 2000, Percentage: 1.5},
-	{Name: "Azerbajan", IsoCode: "AZE", Year: 2021, Percentage: 54.2},
+	{Name: "Azerbaijan", IsoCode: "AZE", Year: 2000, Percentage: 1.5},
+	{Name: "Azerbaijan", IsoCode: "AZE", Year: 2021, Percentage: 54.2},
 	{Name: "Chad", IsoCode: "TCD", Year: 2021, Percentage: 99.1},
 	{Name: "Germany", IsoCode: "DEU", Year: 2000, Percentage: 13.2},
 	{Name: "Germany", IsoCode: "DEU", Year: 2021, Percentage: 42.2},
@@ -20,7 +20,7 @@ var sampleData = []Assignment2.CountryData{
 func TestGetAllCountries(t *testing.T) {
 	// Expected output
 	expected := []Assignment2.CountryData{
-		{Name: "Azerbajan", IsoCode: "AZE", Year: 2021, Percentage: 54.2},
+		{Name: "Azerbaijan", IsoCode: "AZE", Year: 2021, Percentage: 54.2},
 		{Name: "Chad", IsoCode: "TCD", Year: 2021, Percentage: 99.1},
 		{Name: "Germany", IsoCode: "DEU", Year: 2021, Percentage: 42.2},
 		{Name: "Thailand", IsoCode: "THA", Year: 2021, Percentage: 99.1},
@@ -35,7 +35,7 @@ func TestGetAllCountries(t *testing.T) {
 func TestGetOneCountry(t *testing.T) {
 	// Expected output
 	expected := []Assignment2.CountryData{
-		{Name: "Azerbajan", Year: 2021, IsoCode: "AZE", Percentage: 54.2},
+		{Name: "Azerbaijan", Year: 2021, IsoCode: "AZE", Percentage: 54.2},
 		{Name: "Germany", Year: 2021, IsoCode: "DEU", Percentage: 42.2},
 	}
 
