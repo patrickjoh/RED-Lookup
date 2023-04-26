4af7af994c38325fd6e4eb5ed096043859b99ca7
Magnus Johannessen <magnuswj@stud.ntnu.no>
2023-04-26 20:33:14 +0200

    add: commenting for renewHistory_test.go

--- handler/renewHistory_test.go
+++ handler/renewHistory_test.go
@@ -13,6 +13,7 @@ import (
 // Local URL
 const URL = "http://localhost:" + Assignment2.DEFAULT_PORT
 
+// TestHandleHistoryGet Tests main functionality of the history handler via the getFromBeginToEnd function
 func TestHandleHistoryGet(t *testing.T) {
 	// Change current working directory to the directory where the test file is located
 	err := os.Chdir("..")
@@ -47,7 +48,7 @@ func TestHandleHistoryGet(t *testing.T) {
 
 }
 
-// test for not supported method
+// TestMethodNotImplemented tests if the correct statuscode is returned when the method is not implemented
 func TestMethodNotImplemented(t *testing.T) {
 	// Change current working directory to the directory where the test file is located
 	err := os.Chdir("..")
@@ -72,7 +73,7 @@ func TestMethodNotImplemented(t *testing.T) {
 	assert.Equal(t, http.StatusNotImplemented, response.StatusCode)
 }
 
-// Test if handler returns 200 ok with valid request
+// TestFunctionality tests if the function returns 200 OK with correct url
 func TestFunctionalityStatusOK(t *testing.T) {
 	// Change current working directory to the directory where the test file is located
 	err := os.Chdir("..")
@@ -98,7 +99,7 @@ func TestFunctionalityStatusOK(t *testing.T) {
 
 }
 
-// tester for for errorhandling for om isokode som er skrevet inn enten er 3 eller 0
+// TestMalformedURL tests if the function returns 400 bad request with malformed url
 func TestMalformedURL(t *testing.T) {
 	// Change current working directory to the directory where the test file is located
 	err := os.Chdir("..")
@@ -124,6 +125,7 @@ func TestMalformedURL(t *testing.T) {
 
 }
 
+// TestBeginEnd Tests error handling if begin is greater than end and that the correct statuscode is returned
 func TestBeginEnd(t *testing.T) {
 	// Change current working directory to the directory where the test file is located
 	err := os.Chdir("..")
@@ -147,6 +149,7 @@ func TestBeginEnd(t *testing.T) {
 	assert.Equal(t, http.StatusBadRequest, response.StatusCode)
 }
 
+// Tests if the function returns 404 not found with invalid request and no countries are found
 func TestNoCountryFound(t *testing.T) {
 	// Change current working directory to the directory where the test file is located
 	err := os.Chdir("..")
@@ -170,6 +173,7 @@ func TestNoCountryFound(t *testing.T) {
 	assert.Equal(t, http.StatusNotFound, response.StatusCode)
 }
 
+// Test  if the function returns 200 ok with valid request of no isocodes in the url
 func TestNoIsocode(t *testing.T) {
 	// Change current working directory to the directory where the test file is located
 	err := os.Chdir("..")
@@ -193,6 +197,7 @@ func TestNoIsocode(t *testing.T) {
 	assert.Equal(t, http.StatusOK, response.StatusCode)
 }
 
+// TestPercentageSort tests if the function percentageSort returns 200 ok
 func TestPercentageSort(t *testing.T) {
 	// Change current working directory to the directory where the test file is located
 	err := os.Chdir("..")
@@ -216,6 +221,7 @@ func TestPercentageSort(t *testing.T) {
 	assert.Equal(t, http.StatusOK, response.StatusCode)
 }
 
+// TestGetCountriesMean tests if the function getAllCountriesMean returns the correct mean
 func TestGetCountriesMean(t *testing.T) {
 	sampleData := []structs.CountryData{
 		{Name: "Norway", IsoCode: "NOR", Year: 1990, Percentage: 72.44774},
